835c606ee942e2325c319f0010000a55b0c75bf0
nduseja <nduseja@tamu.edu>
2020-04-20 17:05:01 -0700

    Adding joint tool for directed studies

---
+++ diarization/pyAudioAnalysis/analysis_results.py
@@ -0,0 +1,26 @@
+import csv
+
+
+def preprocess_diarization_results(duration, speaker_ids):
+    duration_per_sample = duration/float(len(speaker_ids))
+    speaker_windows_list = []
+    start_time = 0
+    end_time = 0
+    curr_speaker_id = speaker_ids[0]
+    for id in speaker_ids:
+        if curr_speaker_id == id:
+            end_time += duration_per_sample
+            continue
+        else:
+           speaker_windows_list.append((start_time, end_time, curr_speaker_id))
+           start_time = end_time
+           end_time = end_time + duration_per_sample
+           curr_speaker_id = id
+    speaker_windows_list.append((start_time, end_time, curr_speaker_id))
+    print(speaker_windows_list)
+    with open("data/speaker_windows", "w") as the_file:
+        csv.register_dialect("custom", delimiter=" ", skipinitialspace=True)
+        writer = csv.writer(the_file, dialect="custom")
+        for tup in speaker_windows_list:
+            writer.writerow(tup)
+
--- transcription/transcribe_word_time_offsets.py
+++ transcription/transcribe_word_time_offsets.py
@@ -24,7 +24,7 @@ Example usage:
 
 import argparse
 import io
-
+import csv
 
 def transcribe_file_with_word_time_offsets(speech_file):
     """Transcribe the given audio file synchronously and output the word time
@@ -72,28 +72,30 @@ def transcribe_gcs_with_word_time_offsets(gcs_uri):
     audio = types.RecognitionAudio(uri=gcs_uri)
     config = types.RecognitionConfig(
         encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
+        sample_rate_hertz=8000,
         language_code='en-US',
         enable_word_time_offsets=True, model='video')
 
     operation = client.long_running_recognize(config, audio)
-
     print('Waiting for operation to complete...')
     result = operation.result(timeout=200)
-
+    result_time_offsets = []
     for result in result.results:
         alternative = result.alternatives[0]
         print(u'Transcript: {}'.format(alternative.transcript))
         print('Confidence: {}'.format(alternative.confidence))
-
         for word_info in alternative.words:
             word = word_info.word
             start_time = word_info.start_time
+            start = start_time.seconds + start_time.nanos * 1e-9
             end_time = word_info.end_time
-            print('Word: {}, start_time: {}, end_time: {}'.format(
-                word,
-                start_time.seconds + start_time.nanos * 1e-9,
-                end_time.seconds + end_time.nanos * 1e-9))
+            end = end_time.seconds + end_time.nanos * 1e-9
+            result_time_offsets.append((word,start,end))
+        with open("word-time-offsets", "w") as the_file:
+            csv.register_dialect("custom", delimiter=" ", skipinitialspace=True)
+            writer = csv.writer(the_file, dialect="custom")
+            for tup in result_time_offsets:
+                writer.writerow(tup)
 # [END speech_transcribe_async_word_time_offsets_gcs]
 
 
